fe1816fd1a2d996e1c14ac1fc4fea0140c022509
Mahirpatel7 <patelmahir2004@gmail.com>
2025-12-15 23:11:26 +0530

    Netlify commit

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@
 
 
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AppLayout from "./layouts/AppLayout";
 import HomePage from "./pages/HomePage";
 
