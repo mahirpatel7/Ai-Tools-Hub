66b81bd7e50400ea006b4707594893056685f026
Mahirpatel7 <patelmahir2004@gmail.com>
2025-12-20 22:06:19 +0530

    googleFile commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Box } from "@mui/material";
 import AppLayout from "./layouts/AppLayout";
 import HomePage from "./pages/HomePage";
 
